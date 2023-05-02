b554e81a3ca2bab6d21ac761c9925534aaff777f
Christian-cmd-ui <danbel418@gmail.com>
2023-05-02 09:19:07 +0100

    daniel first commit

---
+++ components/NavigateCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+
+const NavigateCard = () => {
+    return (
+        <View style={tw`bg-white flex-1`}>
+            <Text>Yo</Text>
+        </View>
+    )
+}
+
+export default NavigateCard
+
+const styles = StyleSheet.create({})
